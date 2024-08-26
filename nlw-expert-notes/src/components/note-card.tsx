c227204df7eecab5acd094915ac06486cdb1d637
Alois Sailer <alois_sailer@yahoo.com.br>
2024-08-26 20:41:32 -0300

    aula1 completa

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
   return (
-    <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+    <button className="rounded-md text-left outline-none bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
       <span className="text-sm font-medium text-slate-300">há 2 dias</span>
       <p className="text-sm leading-6 text-slate-400">
         No app do NLW vamos criar um layout incrível, assim podemos entregar a
@@ -8,6 +8,6 @@ export function NoteCard() {
         projeto que permite o usuário salvar notas em texto ou áudio.
       </p>
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </div>
+    </button>
   )
 }
